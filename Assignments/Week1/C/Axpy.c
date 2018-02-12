fc41df6ee9b222a963a965e750c8f851b96232f1
Robert van de Geijn <rvdg@cs.utexas.edu>
2018-02-11 20:04:25 -0600

    Creating Assignments subdirectory

---
+++ Assignments/Week1/C/Axpy.c
@@ -0,0 +1,10 @@
+#define chi( i ) x[ (i)*incx ]   // map chi( i ) to array x 
+#define psi( i ) y[ (i)*incy ]   // map psi( i ) to array y
+
+void Axpy( int n, double alpha, double *x, int incx, double *y, int incy )
+{
+  int i;
+
+  for ( i=0; i<n; i++ )
+    psi( i ) =                    ;
+}
