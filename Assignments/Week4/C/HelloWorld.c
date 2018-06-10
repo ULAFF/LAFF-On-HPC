da4991d44e3000c8a36d3a89449ad31ace128d6e
Robert van de Geijn <rvdg@cs.utexas.edu>
2018-06-10 08:20:57 -0500

    Resetting HelloWorld to simplest

--- Assignments/Week4/C/HelloWorld.c
+++ Assignments/Week4/C/HelloWorld.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "omp.h"
-
 int main(int argc, char *argv[])
 {
-  
-#pragma omp parallel 
-  {
-    int nthreads = omp_get_num_threads();
-    int tid = omp_get_thread_num();
-    
-    printf( "Hello World! from %d of %d\n", tid, nthreads );
-  }
+  printf( "Hello World!\n" );
 }
