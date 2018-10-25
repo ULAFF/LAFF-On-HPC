898e56f73a01dda8cc20eedf74b1679733dd54e2
Robert van de Geijn <rvdg@cs.utexas.edu>
2018-10-25 16:39:36 -0500

    Adding prototype for Axpy

--- Assignments/Week1/C/Gemv_J_Axpy.c
+++ Assignments/Week1/C/Gemv_J_Axpy.c
@@ -2,11 +2,11 @@
 #define chi( i )  x[ (i)*incx ]         // map chi( i )  to array x
 #define psi( i )  y[ (i)*incy ]         // map psi( i )  to array x
 
-void Dots( int, const double *, int, const double *, int, double * );
+void Axpy( int, double, double *, int, double *, int );
 
 void Gemv( int m, int n, double *A, int ldA,
            double *x, int incx, double *y, int incy )
 {
   for ( int j=0; j<n; j++ )
-    Axpy(   ,    ,    ,    ,    ,     );
+    Axpy(  ,     ,      ,   ,   ,     );
 }
